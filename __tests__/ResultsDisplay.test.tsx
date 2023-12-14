85ae83b7e51c068ad8ca0673a992ce8b1bc103cc
‘jiun-dev’ <jiun.dev@gmail.com>
2023-12-14 17:40:45 +0900

    test : 퀴즈 결과 컴포넌트

---
+++ __tests__/ResultsDisplay.test.tsx
@@ -0,0 +1,24 @@
+import ResultsDisplay from '../src/components/ResultDisplay';
+import {render} from '@testing-library/react-native';
+import {formatTime} from '../src/utils/formatTime';
+
+describe('ResultsDisplay 컴포넌트 테스트', () => {
+  test('컴포넌트 렌더링 테스트', () => {
+    const totalQuestions = 10;
+    const correctCount = 7;
+    const elapsedTime = 1000;
+
+    const {getByText} = render(
+      <ResultsDisplay
+        totalQuestions={totalQuestions}
+        correctCount={correctCount}
+        elapsedTime={elapsedTime}
+      />,
+    );
+
+    expect(getByText(`총 퀴즈 개수: ${totalQuestions}`)).toBeDefined();
+    expect(getByText(`정답: ${correctCount}`)).toBeDefined();
+    expect(getByText(`오답: ${totalQuestions - correctCount}`)).toBeDefined();
+    expect(getByText(`소요시간: ${formatTime(elapsedTime)}`)).toBeDefined();
+  });
+});
