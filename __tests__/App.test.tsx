c9114170a9757d81a1446be652a62c47961f45ed
‘jiun-dev’ <jiun.dev@gmail.com>
2023-12-14 16:20:46 +0900

    init : jest 관련 라이브러리 추가

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -12,6 +12,11 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-gesture-handler', () => ({
+  ...jest.requireActual('react-native-gesture-handler'),
+  GestureHandlerRootView: 'View',
+}));
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
 };
